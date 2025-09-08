22058306a3a6bf444875928a8bb98c833ec1601a
fratzinger <22286818+fratzinger@users.noreply.github.com>
2025-09-08 09:05:17 +0200

    test: fix tests

--- src/transformers/parse-date/parse-date.transformer.test.ts
+++ src/transformers/parse-date/parse-date.transformer.test.ts
@@ -1,9 +1,11 @@
 import { parseDate } from './parse-date.transformer.js'
 
+const options = { context: {} as any, i: 0 }
+
 describe('transformers/parseDate', () => {
   it('single field', () => {
     const item = { date: '2023-10-01T12:00:00Z' }
-    parseDate('date')(item)
+    parseDate('date')(item, options)
     expect(item).toEqual({ date: new Date('2023-10-01T12:00:00Z') })
   })
 
@@ -12,7 +14,7 @@ describe('transformers/parseDate', () => {
       startDate: '2023-10-01T12:00:00Z',
       endDate: '2023-10-02T12:00:00Z',
     }
-    parseDate(['startDate', 'endDate'])(item)
+    parseDate(['startDate', 'endDate'])(item, options)
     expect(item).toEqual({
       startDate: new Date('2023-10-01T12:00:00Z'),
       endDate: new Date('2023-10-02T12:00:00Z'),
@@ -21,19 +23,19 @@ describe('transformers/parseDate', () => {
 
   it('ignores null or undefined values', () => {
     const item = { date: null, anotherDate: undefined }
-    parseDate(['date', 'anotherDate'])(item)
+    parseDate(['date', 'anotherDate'])(item, options)
     expect(item).toEqual({ date: null, anotherDate: undefined })
   })
 
   it('does not throw if field is missing', () => {
     const item = { date: '2023-10-01T12:00:00Z' }
-    parseDate('missingField')(item)
+    parseDate('missingField')(item, options)
     expect(item).toEqual({ date: '2023-10-01T12:00:00Z' })
   })
 
   it('handles dot notation', () => {
     const item = { event: { startTime: '2023-10-01T12:00:00Z' } }
-    parseDate('event.startTime')(item)
+    parseDate('event.startTime')(item, options)
     expect(item).toEqual({
       event: { startTime: new Date('2023-10-01T12:00:00Z') },
     })
--- src/transformers/trim/trim.transformer.test.ts
+++ src/transformers/trim/trim.transformer.test.ts
@@ -1,38 +1,42 @@
 import { trim } from './trim.transformer.js'
 
+const options = { context: {} as any, i: 0 }
+
 describe('transformers/trim', () => {
   it('single string', () => {
     const item = { name: ' TEST ' }
-    trim('name')(item)
+    trim('name')(item, options)
     expect(item).toEqual({ name: 'TEST' })
   })
 
   it('multiple strings', () => {
     const item = { name: ' TEST ', email: ' TEST@EXAMPLE.COM ' }
-    trim(['name', 'email'])(item)
+    trim(['name', 'email'])(item, options)
     expect(item).toEqual({ name: 'TEST', email: 'TEST@EXAMPLE.COM' })
   })
 
   it('throws error for non-string values', () => {
     const item = { name: 123 }
-    expect(() => trim('name')(item)).toThrow("Expected string (trim 'name')")
+    expect(() => trim('name')(item, options)).toThrow(
+      "Expected string (trim 'name')",
+    )
   })
 
   it('ignores null or undefined values', () => {
     const item = { name: null, email: undefined }
-    trim(['name', 'email'])(item)
+    trim(['name', 'email'])(item, options)
     expect(item).toEqual({ name: null, email: undefined })
   })
 
   it('does not throw if field is missing', () => {
     const item = { name: 'Test' }
-    trim('missingField')(item)
+    trim('missingField')(item, options)
     expect(item).toEqual({ name: 'Test' })
   })
 
   it('handles dot.notation', () => {
     const item = { user: { name: ' TEST ' } }
-    trim('user.name')(item)
+    trim('user.name')(item, options)
     expect(item).toEqual({ user: { name: 'TEST' } })
   })
 })
